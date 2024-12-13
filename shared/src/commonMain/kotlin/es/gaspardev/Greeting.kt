328a557dbc9d82935378034e90d03a796009db3a
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-12-13 12:48:52 +0100

    remove(Global): auto generate initProyect files

--- shared/src/commonMain/kotlin/es/gaspardev/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package es.gaspardev
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/es/gaspardev/Platform.kt
+++
@@ -1,7 +0,0 @@
-package es.gaspardev
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
